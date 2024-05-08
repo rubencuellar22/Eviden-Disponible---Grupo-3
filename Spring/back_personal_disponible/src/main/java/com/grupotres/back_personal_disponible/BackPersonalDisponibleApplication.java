0e8b97fd81850b6c36e226ae46737a0853eadbf3
AlvaroHuertasEviden <alvaro.huertas.external@eviden.com>
2024-05-08 13:19:40 +0200

    Added Base Project, front and back

---
+++ Spring/back_personal_disponible/src/main/java/com/grupotres/back_personal_disponible/BackPersonalDisponibleApplication.java
@@ -0,0 +1,13 @@
+package com.grupotres.back_personal_disponible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPersonalDisponibleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPersonalDisponibleApplication.class, args);
+    }
+
+}
---
+++ Spring/back_personal_disponible/src/test/java/com/grupotres/back_personal_disponible/BackPersonalDisponibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupotres.back_personal_disponible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPersonalDisponibleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
