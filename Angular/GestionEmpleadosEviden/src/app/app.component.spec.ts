c2b47246477ba81bab8c3dcde72abe9c7589b634
Eider Fernandez Crespo <eider.fernandez.external@eviden.com>
2024-05-10 09:16:38 +0200

    Angular

--- Angular/GestionEmpleadosEviden/src/app/app.component.spec.ts
+++ Angular/GestionEmpleadosEviden/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'front_personal_disponible' title`, () => {
+  it(`should have as title 'GestionEmpleadosEviden'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front_personal_disponible');
+    expect(app.title).toEqual('GestionEmpleadosEviden');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, front_personal_disponible');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, GestionEmpleadosEviden');
   });
 });
