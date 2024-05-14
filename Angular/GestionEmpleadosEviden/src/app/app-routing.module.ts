8edc7e707e4fa9c234fcf07ad13ea98f65ede815
Laura-Salgado <laura.salgado.external@eviden.com>
2024-05-14 14:50:01 +0200

    add details

--- Angular/GestionEmpleadosEviden/src/app/app-routing.module.ts
+++ Angular/GestionEmpleadosEviden/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '/', component: AppComponent },
+
+  { path: 'details', component: DetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/GestionEmpleadosEviden/src/app/app.module.ts
+++ Angular/GestionEmpleadosEviden/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FiltersComponent } from './filters/filters.component';
 import { FilteredTableComponent } from './filtered-table/filtered-table.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FiltersComponent,
-    FilteredTableComponent
+    FilteredTableComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
