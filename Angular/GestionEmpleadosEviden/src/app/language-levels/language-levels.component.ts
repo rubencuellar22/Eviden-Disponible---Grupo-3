67163d8a671b90d5570b96e7094a302d95a9c8a4
angarciaeviden <168087472+angarciaeviden@users.noreply.github.com>
2024-05-21 16:39:02 +0200

    Componentes de nivelesss, idioma y programación

---
+++ Angular/GestionEmpleadosEviden/src/app/language-levels/language-levels.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-levels',
+  templateUrl: './language-levels.component.html',
+  styleUrl: './language-levels.component.css'
+})
+export class LanguageLevelsComponent {
+
+}
