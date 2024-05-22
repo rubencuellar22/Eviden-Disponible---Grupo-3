a0d84a6fc394dc5952766a931f806670d48b793f
AlejandroMartinezVega <alejandro.martinez.external@eviden.com>
2024-05-22 13:27:20 +0200

    busqueda primer filtro

--- Angular/GestionEmpleadosEviden/src/app/filters/filters.component.ts
+++ Angular/GestionEmpleadosEviden/src/app/filters/filters.component.ts
@@ -1,24 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Empleado } from '../classes/empleado';
+
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.css']
 })
-export class FiltersComponent {
-  tags: string[] = [];
+export class FiltersComponent implements OnInit {
   newTag: string = '';
+  tags: string[] = [];
+  empleados: Empleado[] = [];
 
-  showChild1: boolean = true; 
+  constructor(private http: HttpClient) {}
 
-  removeTag(tag: string) {
+  ngOnInit(): void {}
+
+  addTag(): void {
+    if (this.newTag && !this.tags.includes(this.newTag)) {
+      this.tags.push(this.newTag);
+      this.newTag = '';
+    }
+  }
+
+  removeTag(tag: string): void {
     this.tags = this.tags.filter(t => t !== tag);
   }
 
-  addTag() {
-    if (this.newTag && !this.tags.includes(this.newTag)) {
-      this.tags.push(this.newTag);
-      this.newTag = ''; // Limpiar el input después de agregar el tag
+  applyFilter(): void {
+    const ciudadTag = this.tags.find(tag => tag.toLowerCase() === 'madrid');
+    console.log("hola");
+    if (ciudadTag) {
+      console.log("Aqui llega");
+      this.http.get<Empleado[]>(`http://localhost:8080/empleado/ciudad/${ciudadTag}`).subscribe(
+        (data: Empleado[]) => {
+          this.empleados = data;
+        },
+        error => {
+          console.error('Error al buscar empleados:', error);
+        }
+      );
     }
   }
 }
