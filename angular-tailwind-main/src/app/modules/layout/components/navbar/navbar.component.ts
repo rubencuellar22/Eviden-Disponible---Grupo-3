3873dbfa38bd718ce6c79c60e1022522f1296b28
AlexEviden1 <alejandro.ramirez.external@eviden.com>
2024-05-31 14:54:39 +0200

    Update navbar.component.ts

--- angular-tailwind-main/src/app/modules/layout/components/navbar/navbar.component.ts
+++ angular-tailwind-main/src/app/modules/layout/components/navbar/navbar.component.ts
@@ -34,7 +34,6 @@ export class NavbarComponent implements OnInit {
   filterTags: string[] = [];
   filterComponent: string[] = [];
   empleados: Empleado[] = [];
-  empleadosFilter: Empleado[] = [];
   jobTechnologyProfile: JobTechnologyProfile[] = [];
   skTechSkill: SkTechSkill[] = [];
   endpoint: string = '';
@@ -76,166 +75,23 @@ export class NavbarComponent implements OnInit {
       this.getFunction(this.endpoint);
     }
   }
-
-  // updateEmployeeList(): void {
-  //   // Filtramos los empleados según los filtros seleccionados
-  //   let filteredEmployees = this.empleadosFilter;
-  //   this.filterTags.forEach(tag => {
-  //     filteredEmployees = filteredEmployees.filter(empleado => empleado.ciudad === tag);
-  //   });
-  //   // Asignamos los empleados filtrados a la lista principal de empleados
-  //   this.empleados = filteredEmployees;
-  // }
   
 
   getFunction(endpoint: string): void {
-    if(this.empleadosFilter.length === 0) {
       this.http.get<Empleado[]>(endpoint).subscribe(
         (data: Empleado[]) => {
           this.empleados = data;
-          this.empleadosFilter = this.empleados;
           console.log(this.empleados);
           this.dataService.changeData(this.empleados);
 
-          localStorage.setItem('empleadosFiltrados', JSON.stringify(this.empleados));
-          // Aquí llamamos a la función para actualizar la lista de empleados después de obtener los datos del servidor
-          //this.updateEmployeeList();
         },
         (error) => {
           console.error('Error al buscar empleados:', error);
           this.errorMessage = 'Error fetching data.';
         },
       );
-    }
   }
 
-  sendData() {
-    
-  }
-
-  applyFilter(): void {
-    this.errorMessage = '';
-    if (!this.selectedItem) {
-      this.errorMessage = 'Please select a filter.';
-      return;
-    }
-    if (this.tags.length === 0) {
-      this.errorMessage = 'Please add at least one tag.';
-      return;
-    }
-
-    // console.log(this.tags);
-    // console.log(this.filterTags);
-    // console.log(this.empleadosFilter.length);
-
-    this.empleados = [];
-
-    for (let i = 0; i < this.filterTags.length; i++) {
-      if (this.empleadosFilter.length != 0) {
-        console.log(this.empleados);
-        for (let empleado of this.empleadosFilter) {
-          switch (this.filterTags[i]) {
-            case 'status':
-              if ((empleado.status = this.tags[i])) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'bench':
-              // if(empleado.bench = this.tags[i]){
-              //   this.empleados.push(empleado);
-              // }
-              break;
-            case 'ciudad':
-              if ((empleado.ciudad = this.tags[i])) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'jornada':
-              if ((empleado.jornada = parseInt(this.tags[i]))) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'grupo':
-              if (empleado.grupo.grupos.includes(this.tags[i])) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'n4':
-              if ((empleado.n4 = this.tags[i])) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'categoria':
-              if ((empleado.categoria = this.tags[i])) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'scr':
-              if ((empleado.scr = parseInt(this.tags[i]))) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'job_technology':
-              if ((empleado.jobTechnology = this.tags[i])) {
-                this.empleados.push(empleado);
-              }
-              break;
-            case 'sk_bus_skill':
-              for (let sk_bus_skill of empleado.skBusSkills) {
-                if (sk_bus_skill.skBusSkill.includes(this.tags[i])) {
-                  this.empleados.push(empleado);
-                }
-              }
-              break;
-            case 'sk_tecnology':
-              for (let sk_technology of empleado.skTechnologies) {
-                if (sk_technology.sktechnology.includes(this.tags[i])) {
-                  this.empleados.push(empleado);
-                }
-              }
-              break;
-            case 'sk_certif':
-              for (let sk_certif of empleado.skCertifs) {
-                if (sk_certif.skCertif.includes(this.tags[i])) {
-                  this.empleados.push(empleado);
-                }
-              }
-              break;
-            case 'sk_lenguage':
-              for (let skLenguage of empleado.skLenguages) {
-                if (skLenguage.sklenguage.includes(this.tags[i])) {
-                  this.empleados.push(empleado);
-                }
-              }
-              break;
-            case 'sk_method':
-              for (let sk_method of empleado.skMethods) {
-                if (sk_method.skmethod.includes(this.tags[i])) {
-                  this.empleados.push(empleado);
-                }
-              }
-              break;
-            case 'sk_tecskill':
-              for (let skTechSkills of empleado.skTechSkills) {
-                if (skTechSkills.skTechSkill.includes(this.tags[i])) {
-                  this.empleados.push(empleado);
-                }
-              }
-              break;
-
-            default:
-              console.error('Filtro no reconocido:', this.selectedItem);
-              this.errorMessage = 'Unrecognized filter selected.';
-              return;
-          }
-        }
-      }
-    }
-
-    localStorage.setItem('empleado', JSON.stringify(this.empleados));
-    const empleadosJSON = JSON.stringify(localStorage.getItem('empleado'));
-    console.log(empleadosJSON);
-  }
 
   removeFilterTag(tag: string): void {
     const index = this.filterTags.indexOf(tag);
